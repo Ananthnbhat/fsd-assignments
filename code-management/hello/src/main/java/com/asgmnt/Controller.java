18f264b63201f1ca9a748b6f4ca9a76be3eebee0
d910124 <Ananth.Bhat@team.telstra.com>
2019-05-13 14:56:31 +0530

    commit all files related to project

---
+++ code-management/hello/src/main/java/com/asgmnt/Controller.java
@@ -0,0 +1,16 @@
+package com.asgmnt;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/app")
+    @ResponseBody
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
