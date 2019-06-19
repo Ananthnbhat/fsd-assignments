111bf794c83651816164c602069f93c6c6a3ed28
Ananth <Ananth.Bhat@team.telstra.com>
2019-06-19 18:29:14 +0530

    refactor table in AddNewVideo

--- 2. ux-assignment/react-video-player/src/components/AddNewVideo/AddNewVideo.js
+++ 2. ux-assignment/react-video-player/src/components/AddNewVideo/AddNewVideo.js
@@ -13,46 +13,50 @@ export default class AddNewVideo extends React.Component {
                     <button>Add Video</button>
                 </div>
                 <table>
-                    <tr>
-                        <th>S.no</th>
-                        <th>Title</th>
-                        <th>URL</th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Bunny story</td>
-                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
-                        <td>Edit</td>
-                        <td>Delete</td>
-                        <td>Approve</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Bunny story</td>
-                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
-                        <td>Edit</td>
-                        <td>Delete</td>
-                        <td>Approve</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Bunny story</td>
-                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
-                        <td>Edit</td>
-                        <td>Delete</td>
-                        <td>Approve</td>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Bunny story</td>
-                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
-                        <td>Edit</td>
-                        <td>Delete</td>
-                        <td>Approve</td>
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>S.no</th>
+                            <th>Title</th>
+                            <th>URL</th>
+                            <th></th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Bunny story</td>
+                            <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                            <td>Approve</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Bunny story</td>
+                            <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                            <td>Approve</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Bunny story</td>
+                            <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                            <td>Approve</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Bunny story</td>
+                            <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                            <td>Edit</td>
+                            <td>Delete</td>
+                            <td>Approve</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         );
