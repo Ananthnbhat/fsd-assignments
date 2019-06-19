d4bed1d720d182f7a7a034007c67d466869a61ed
Ananth <Ananth.Bhat@team.telstra.com>
2019-06-19 18:26:24 +0530

    Add new video component init commit

--- 2. ux-assignment/react-video-player/src/components/AddNewVideo/AddNewVideo.js
+++ 2. ux-assignment/react-video-player/src/components/AddNewVideo/AddNewVideo.js
@@ -1,7 +1,60 @@
 import React from "react";
+import './AddNewVideo.css'
 
-export default function AddNewVideo() {
-    return (
-        <h2>AddNewVideo</h2>
-    );
+export default class AddNewVideo extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <label>Title</label>
+                    <input type="text"></input>
+                    <label>URL</label>
+                    <input type="text" />
+                    <button>Add Video</button>
+                </div>
+                <table>
+                    <tr>
+                        <th>S.no</th>
+                        <th>Title</th>
+                        <th>URL</th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Bunny story</td>
+                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                        <td>Approve</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Bunny story</td>
+                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                        <td>Approve</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Bunny story</td>
+                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                        <td>Approve</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Bunny story</td>
+                        <td>http://media.w3.org/2010/05/bunny/trailer.mp4</td>
+                        <td>Edit</td>
+                        <td>Delete</td>
+                        <td>Approve</td>
+                    </tr>
+                </table>
+            </div>
+        );
+    }
 }
