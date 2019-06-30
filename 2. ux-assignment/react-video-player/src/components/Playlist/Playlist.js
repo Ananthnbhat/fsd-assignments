0c385fd446f6fb322b7a65c814b8bde14f7bc063
Ananthnbhat <ananthnbhat2@gmail.com>
2019-06-30 14:23:28 +0530

    style changes

--- 2. ux-assignment/react-video-player/src/components/Playlist/Playlist.js
+++ 2. ux-assignment/react-video-player/src/components/Playlist/Playlist.js
@@ -9,19 +9,21 @@ class Playlist extends React.Component {
 
     render() {
         const echoStyle = {
-            textAlign: 'center'
+            textAlign: 'center',
+            color: 'black'
         };
         return (
             <div className="playlist">
-                <div className="echo" style={echoStyle}></div>
-                <h3 style={echoStyle}>Playlist</h3>
-                <ul>
-                    {this.props.jsonInfo.map((item, index) => item.approved === 1 ? <p key={index} className="videos" id="video0" onClick={() => this.playVideo(item.url)}><button onClick={() => this.playVideo(item.url)} type="button" className="btn btn-default btn-xs playListButton">
-                        <span className="glyphicon glyphicon-play"></span>
-                    </button>
-                        {item.title}
-                    </p> : null)}
-                </ul>
+                <div className="playlistChild">
+                    <h3 style={echoStyle}>Playlist</h3>
+                    <ul>
+                        {this.props.jsonInfo.map((item, index) => item.approved === 1 ? <p key={index} className="videos" id="video0" onClick={() => this.playVideo(item.url)}><button onClick={() => this.playVideo(item.url)} type="button" className="btn btn-xs playListButton btn-primary-outline">
+                            <span className="glyphicon glyphicon-play"></span>
+                        </button>
+                            {item.title}
+                        </p> : null)}
+                    </ul>
+                </div>
             </div>
         );
     }
